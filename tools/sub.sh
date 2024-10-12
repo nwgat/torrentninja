923bf8b241474ae78f8980255044f2910ae3b8f3
nwgat <github@nwgat.net>
2024-10-12 04:40:35 +0200

    Update sub.sh
    silent ffmpeg output, no mkv in output

--- tools/sub.sh
+++ tools/sub.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <input_file>"
     exit 1
 fi
 
 input_file="$1"
-output_file="${input_file}.default.en.srt"
+output_file="${input_file%.mkv}.default.en.srt"
 
 # Run the ffmpeg command
-if ffmpeg -hide_banner -i "$input_file" -map 0:s:0 "$output_file"; then
+if ffmpeg -hide_banner -i "$input_file" -map 0:s:0 "$output_file" > /dev/null 2>&1; then
     echo "Subtitle file '$output_file' created successfully."
 else
     echo "Error executing ffmpeg command."
