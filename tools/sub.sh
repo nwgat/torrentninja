4f888f0800b387d3c6f9cf65841b0b2cf6fd1ba0
nwgat <github@nwgat.net>
2024-10-13 02:20:53 +0200

    Update sub.sh
    fix some text now showing up during playback

--- tools/sub.sh
+++ tools/sub.sh
@@ -8,7 +8,7 @@ input_file="$1"
 output_file="${input_file%.mkv}.default.en.srt"
 
 # Run the ffmpeg command
-if ffmpeg -hide_banner -i "$input_file" -map 0:s:0 "$output_file" > /dev/null 2>&1; then
+if ffmpeg -hide_banner -i "$input_file" -map 0:s:0 -codec:s text "$output_file" > /dev/null 2>&1; then
     echo "Subtitle file '$output_file' created successfully."
 else
     echo "Error executing ffmpeg command."
